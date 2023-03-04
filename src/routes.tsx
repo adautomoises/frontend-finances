c790d53a1ad4ece4d8c8703c8baa98b20ea39aa3
adautomoises <adautomaleandro@gmail.com>
2023-03-03 22:10:09 -0300

    Rotas

---
+++ src/routes.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import { DashboardLayout } from "./layouts/DashboardLayout";
+
+import { Home } from "./pages/Home";
+import { Dashboard } from "./pages/Dashboard";
+import { Spending } from "./pages/Spending";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastrar" element={<SignUp />} />
+        <Route path="/entrar" element={<SignIn />} />
+        <Route path="/" element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/gastos" element={<Spending />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
